a91c5c0a53e1e92b4a38404a95202839b078bf42
Bo Martin <bmartin@lirio.co>
2025-07-01 09:52:35 -0400

    Point to different Kafka docker compose files

--- zshrc.d/kafka.zsh
+++ zshrc.d/kafka.zsh
@@ -1,9 +1,8 @@
-export CONFLUENT_HOME=/Users/bomartin/swtools/confluent-7.4.0
-# export CONFLUENT_CURRENT=/Users/bomartin/swtools/confluent-5.5.4/var
+export CONFLUENT_HOME=/Users/bmartin/swtools/confluent-7.4.0
 export PATH="${CONFLUENT_HOME}/bin:$PATH"
-alias kstat='docker-compose -f ~/swtools/kafka-stack-docker-compose/zk-single-kafka-single.yml ps'
+alias kstat='docker compose -f ~/swtools/kafka-stack-docker-compose/single-7.4.0.yml ps'
 # alias kstart='confluent local services kafka start'
-alias kstart='cd ~/swtools/kafka-stack-docker-compose/ && docker-compose -f zk-single-kafka-single.yml up --detach; cd -'
+alias kstart='cd ~/swtools/kafka-stack-docker-compose/ && docker compose -f single-7.4.0.yml up --detach; cd -'
 # alias kstop='confluent local services kafka stop'
-alias kstop='cd ~/swtools/kafka-stack-docker-compose/ && docker-compose -f zk-single-kafka-single.yml down'
+alias kstop='cd ~/swtools/kafka-stack-docker-compose/ && docker compose -f single-7.4.0.yml down; cd -'
 alias ktopic='kafka-topics --zookeeper localhost:2181'
